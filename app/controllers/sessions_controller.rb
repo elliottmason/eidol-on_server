2bb0c761f63a55f0142fc7cb22cf12a151e2092d
R. Elliott Mason <eleo@fastmail.fm>
2019-10-14 16:53:52 -0400

    Slightly more dynamic redirect after "sign in"

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,14 @@
+# frozen_string_literal: true
+
 class SessionsController < ApplicationController
   def new
-    Rails.logger.debug(cookies.signed[:account_id])
   end
 
   def create
     account = Account.find_by_email_address(params[:email_address])
     cookies.signed[:account_id] = { value: account.id, expires: 3.days }
 
-    Rails.logger.debug(cookies.signed[:account_id])
-
-    redirect_to 'http://localhost:3000/matches/1'
+    match_id = Match.last.id
+    redirect_to "http://localhost:3000/matches/#{match_id}"
   end
 end
