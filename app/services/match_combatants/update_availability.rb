ee5d607c61f7cb41f55aa448c2169c625aaabf87
R. Elliott Mason <eleo@fastmail.fm>
2019-10-21 23:34:24 -0400

    I wish my past, present, and future selves could decide on naming schemes

--- app/services/match_combatants/update_availability.rb
+++ app/services/match_combatants/update_availability.rb
@@ -4,9 +4,9 @@ module MatchCombatants
   # This basically just K.O.'s [MatchCombatant]s and removes them from the board
   # when their remaining_health drops too low by inserting a new status
   class UpdateAvailability < ApplicationService
-    # @param match_combatant [MatchCombatant]
-    def initialize(match_combatant:)
-      @match_combatant = match_combatant
+    # @param combatant [MatchCombatant]
+    def initialize(combatant:)
+      @combatant = combatant
     end
 
     def perform
@@ -23,9 +23,9 @@ module MatchCombatants
     private
 
     # @return [MatchCombatant]
-    attr_reader :match_combatant
+    attr_reader :combatant
 
     # @return [MatchCombatantStatus]
-    delegate :status, to: :match_combatant
+    delegate :status, to: :combatant
   end
 end
