5a7606ed52d54decd58e3d2a6959710c94c3c456
R. Elliott Mason <eleo@fastmail.fm>
2019-10-27 19:00:11 -0400

    Verifies that relocation position is in range

--- app/services/matches/process_relocation.rb
+++ app/services/matches/process_relocation.rb
@@ -23,11 +23,26 @@ module Matches
 
     # @return [Boolean]
     def allowed?
-      match_combatant.status.availability == 'available'
+      match_combatant_position = match_combatant.status.board_position
+      x_coord = match_combatant_position.x
+      y_coord = match_combatant_position.y
+      move_range = move_turn_effect.move_turn.move.range
+      x_range = (x_coord - move_range)..(x_coord + move_range)
+      y_range = (y_coord - move_range)..(y_coord + move_range)
+
+      available_board_position =
+        BoardPosition \
+        .for_match(match) \
+        .where(id: board_position.id, x: x_range, y: y_range) \
+        .first
+
+      match_combatant.queued? &&
+        available_board_position&.occupants&.empty?
     end
 
     # @return [MatchEvent]
     def perform
+      @successful = false
       ActiveRecord::Base.transaction do
         create_match_event
         MatchCombatants::Relocate.with(
@@ -35,7 +50,14 @@ module Matches
           match_combatant: match_combatant,
           match_event: match_event
         )
+        @successful = true
       end
+    end
+
+    # @return [nil]
+    def after_failure
+      Rails.logger.debug(' --- FAILED --- ')
+      create_match_event('failed')
     end
 
     private
@@ -55,8 +77,9 @@ module Matches
     # @return [MoveTurnEffect]
     attr_reader :move_turn_effect
 
+    # @param status [String]
     # @return [MatchEvent]
-    def create_match_event
+    def create_match_event(status = 'successful')
       @match_event =
         MatchEvent.create!(
           board_position: board_position,
@@ -65,8 +88,13 @@ module Matches
           move_turn_effect: move_turn_effect,
           category: 'relocation',
           property: 'normal',
-          status: 'successful'
+          status: status
         )
+    end
+
+    # @return [Match]
+    def match
+      @match ||= board_position.match
     end
   end
 end
