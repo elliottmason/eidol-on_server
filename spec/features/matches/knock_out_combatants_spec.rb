881559d0f2d073916e2888a02aa92eaeb004ba62
R. Elliott Mason <eleo@fastmail.fm>
2020-01-02 19:57:14 -0500

    False positive since we never set comparative speeds

--- spec/features/matches/knock_out_combatants_spec.rb
+++ spec/features/matches/knock_out_combatants_spec.rb
@@ -1,14 +1,17 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
+# rubocop:disable Metrics/BlockLength
 describe 'Knocked out combatants' do
   let(:accounts) { FactoryBot.create_list(:account, 2, :with_combatants) }
-  let(:match) { Matches::Create.for(accounts: accounts).match }
+  let!(:match) { Matches::Create.for(accounts: accounts).match }
   let(:player_a) { match.players.first }
   let(:player_b) { match.players.last }
-  let(:source_combatant) { player_a.combatants.first }
-  let(:target_combatant) { player_b.combatants.first }
+  let!(:source_combatant) { player_a.combatants.first }
+  let!(:target_combatant) { player_b.combatants.first }
 
-  let(:move) { Move.find_by_name('Direct Hit') }
+  let(:move) { Move.find_by(name: 'Direct Hit') }
   let(:source_match_combatants_move) do
     MatchCombatantsMove.where(
       match_combatant: source_combatant,
@@ -22,10 +25,18 @@ describe 'Knocked out combatants' do
     ).first
   end
 
-  let(:source_position) { match.board_positions.where(x: 2, y: 1).first}
-  let(:target_position) { match.board_positions.where(x: 2, y: 2). first }
+  let(:source_position) { match.board_positions.where(x: 2, y: 1).first }
+  let(:target_position) { match.board_positions.where(x: 2, y: 2).first }
 
   before do
+    # Lower remaining_health down to 1
+    [source_combatant, target_combatant].each do |combatant|
+      amount = 1 - combatant.maximum_health
+      MatchCombatants::AdjustRemainingHealth.with(
+        amount: amount, match_combatant: combatant
+      )
+    end
+
     MatchCombatants::Deploy.with(
       board_position: source_position,
       match_combatant: source_combatant
@@ -46,17 +57,11 @@ describe 'Knocked out combatants' do
       board_position: source_position,
       match_combatants_move: target_match_combatants_move
     )
-    # which should cause the turn to be processed automatically?
-
-    # at which point their respective attacks should run
-    # and target_combatant should be KO'd
   end
 
   it 'moves are not processed or applied' do
-    puts target_combatant.remaining_health
-    puts target_combatant.maximum_health
-    puts '---'
     expect(target_combatant).to be_knocked_out
-    expect(source_combatant.remaining_health).to eq source_combatant.maximum_health
+    expect(source_combatant.remaining_health).to eq(1)
   end
 end
+# rubocop:enable Metrics/BlockLength
